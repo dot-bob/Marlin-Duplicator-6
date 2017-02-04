4dad73aa4e0291c2ac7bd6a268810c9f8962029d
1bigpig <bwclark1@gmail.com>
2017-02-04 05:01:46 -0600

    Update Configuration.h
    Current extruder step default setting (99.1) is different from Wanhao's default setting (94.3).  Reverted to Wanhao's setting of 94.3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -500,7 +500,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0395, 80.0395, 400.48, 99.1 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0395, 80.0395, 400.48, 94.3 }
 
 /**
  * Default Max Feed Rate (mm/s)
